a08a8fa154c4bb35dfe926da277bcb427c871d22
Sergey <sergey_1c@mail.ru>
2021-02-23 16:00:22 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SmallBasic.Library;
+
+//Создаем графический интрефес для игры в тетрис
+
+namespace TetrisGui
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Turtle.Show();
+            Turtle.Move(30);
+            Turtle.TurnLeft();
+            Turtle.Move(50);
+        }
+    }
+}
